14a4c2b623186e1f9c941233dab8c71c1fa4b66d
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-07-23 15:53:21 -0400

    Primer commit

---
+++ src/main/java/com/example/bootcrudpostgresqlback/BootCrudPostgresqlBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootcrudpostgresqlback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCrudPostgresqlBackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BootCrudPostgresqlBackApplication.class, args);
+  }
+
+}
